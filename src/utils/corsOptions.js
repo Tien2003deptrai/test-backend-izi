783702ef4733e31ab49236284f1a39c9f3dd1d56
Tien2003deptrai <duongvantiendtu@gmail.com>
2024-09-06 10:13:14 +0700

    first commi2t

--- src/utils/corsOptions.js
+++
@@ -1,8 +0,0 @@
-exports.corsOptions = {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-};
-
-
